725585eae53e82ab7381bca2ffef10d292e692fe
Stefanie Böhme <s.boehme@thisisdmg.com>
2014-09-04 12:54:24 +0200

    added image precompile for header

--- lib/muffins/buttercream/engine.rb
+++ lib/muffins/buttercream/engine.rb
@@ -6,6 +6,7 @@ module Muffins
     class Engine < ::Rails::Engine
       initializer 'muffins-buttercream.load_config_initializers', after: 'setup_sass', group: :all do |app|
         app.config.sass.load_paths << config.root.join('vendor', 'assets')
+        app.config.assets.precompile << "muffins/themes/buttercream/header_bg.png"
       end
     end
   end
